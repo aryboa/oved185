0064cf838b1867de4bc9307c1e850d5c37fe1316
Administrator <Administrator@192.168.12.114>
2018-12-13 11:26:16 +0100

    Riaggiunta Package Alessio dopo ReFork

---
+++ oved185/src/Alessio/zoo/Rhino.java
@@ -0,0 +1,8 @@
+package Alessio.zoo;
+
+public class Rhino extends Animal {
+	public Rhino(String name) {
+		super(name);                            //mi richiama il costruttore di animal passsandogli una stringa
+	}
+
+}
